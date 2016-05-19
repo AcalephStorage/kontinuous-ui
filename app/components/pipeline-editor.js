49d8b5332068618b7e5c0dd571b849d6df699aeb
Stefany Serino <stefanyserino@gmail.com>
2016-05-19 11:27:08 +0800

    Add 'pull request' option for committing updated definition file
    Remove definition model

--- app/components/pipeline-editor.js
+++ app/components/pipeline-editor.js
@@ -8,37 +8,105 @@ export default Ember.Component.extend({
 
   pipeline: Ember.inject.service(),
   store: Ember.inject.service(),
+  session: Ember.inject.service(),
   notify: Ember.inject.service(),
 
-  definition: null,
-  commit: {
-    message: "",
-    option: "commit",
+  willPullRequest: Ember.computed.equal('commitOption.option', 'pull_request'),
+
+  init() {
+    this._super(...arguments);
+    this.resetForm();
+    this.performTask(this.get('definitionFetcher'));
+  },
+
+  didRender() {
+    if (this.$('input[name="commitOption"][checked="checked"]').length === 0) {
+      let commitOption = this.get('commitOption.option');
+      this.$(`input[name="commitOption"][value="${commitOption}"]`).attr("checked", "checked")
+    }
   },
 
   actions: {
     close() {
       this.sendAction('close');
     },
+    selectCommitOption(key) {
+      this.set('commitOption.option', key);
+      if (key !== 'pull_request') {
+        this.set("commitOption.branch_name", "");
+      }
+    },
     updateDefinition() {
-      this.get('definitionUpdater').perform();
+      this.performTask(this.get('definitionUpdater'));
     }
   },
 
-  definitionFetcher: task(function*() {
-    let query = this.get('model').getProperties('repo', 'owner');
-    let definition = yield this.get('store').queryRecord('definition', query);
-    this.set('definition', definition);
-  }).on('init').drop(),
+  resetForm() {
+    this.set('commitOptions', {
+      commit: "Commit directly to the default branch",
+      pull_request: "Create a new branch for this commit and start a pull request",
+    });
+    this.set('commitOption', {
+      option: "commit",
+      message: "",
+      branch_name: ""
+    });
+  },
 
-  definitionUpdater: task(function*() {
-    this.set('definition.pipeline', this.get('model'));
-    yield this.get('definition').save()
-      .then(() => {
-        this.get('notify').success('Successfully updated `.pipeline.yml`.');
+  performTask(task) {
+    this.get('session').authorize('authorizer:kontinuous', (headerName, headerValue) => {
+      let api = Configuration.APP.kontinuousAPI;
+      let params = this.get('model').getProperties('owner', 'repo');
+      let headers = {};
+      headers[headerName] = headerValue;
+      let url = `${api.host}/api/${api.version}/pipelines/${params.owner}/${params.repo}/definition`;
+      task.perform({headers: headers, url: url});
+    });
+  },
+
+  serializeDefinition(definition) {
+    let currentDefinition = this.get('definition') || {};
+    if (definition && definition.content) {
+      definition.decodedContent = atob(definition.content);
+    }
+    Ember.assign(currentDefinition, definition);
+    this.set('definition', currentDefinition);
+  },
+
+  normalizeDefinition() {
+    let definition = this.get('definition');
+    return {
+      content: btoa(this.get('definition.decodedContent')),
+      sha: this.get('definition.sha')
+    }
+  },
+
+  definitionFetcher: task(function*(ajaxOptions) {
+    yield Ember.$.ajax(ajaxOptions)
+      .then((definition) => {
+        this.serializeDefinition(definition);
+      });
+  }).drop(),
+
+  definitionUpdater: task(function*(ajaxOptions) {
+    let definition = this.normalizeDefinition();
+
+    Ember.assign(ajaxOptions, {
+      type: 'PUT',
+      data: JSON.stringify({
+        definition: definition,
+        commit: this.get('commitOption')
+      }),
+      contentType: 'application/json;chartset=UTF-8'
+    });
+
+    yield Ember.$.ajax(ajaxOptions)
+      .then((definition) => {
+        this.get('notify').success("Successfully updated the definition file for the pipeline");
+        this.serializeDefinition(definition);
       }, (res) => {
-        this.get('notify').error(res.errors.Message || 'Failed to update `.pipeline.yml`.');
-      })
+        this.get('notify').error(res.responseJSON.Message || "Failed to update the definition file for the pipeline");
+      });
   }).drop(),
 
 });
