daddc87bc118ecece6bcc0cff0c1b1efeb5a8678
Stefany Serino <stefanyserino@gmail.com>
2016-05-20 14:42:33 +0800

    Decode (base64) logs content

--- app/models/log.js
+++ app/models/log.js
@@ -4,5 +4,5 @@ import attr from 'ember-data/attr';
 export default Model.extend({
   filename: attr('string'),
   title: attr('string'),
-  content: attr('string'),
+  content: attr('base64-string'),
 });
